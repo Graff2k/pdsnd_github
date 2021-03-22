9e781d59577797048572db02e30ef5ea52b61fc4
Louie VanderMolen <lv9437@att.com>
2021-03-22 00:21:27 -0500

    Add a more polite when wanting to restart

--- bikeshare.py
+++ bikeshare.py
@@ -208,7 +208,7 @@ def main():
         user_stats(df)
         data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no. \n')
+        restart = input('\nWould you like to restart? Please enter yes or no. \n')
         if restart.lower() != 'yes':
             break
 
